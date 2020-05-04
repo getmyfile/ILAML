6271694619da1136edfb2a300421b445168df1d1
getmyfile <55376959+getmyfile@users.noreply.github.com>
2020-05-04 13:24:39 +0530

    Update __init__.py

--- ilaml/__init__.py
+++ ilaml/__init__.py
@@ -1,5 +1,5 @@
 from .inference_models.hierarchical_model import infer_labels
-from .utils.dataset import GogglesDataset
+from .utils.dataset import ILAMLDataset
 from .affinity_matrix_construction.construct import construct_image_affinity_matrices
 
 __version__ = '0.1'
